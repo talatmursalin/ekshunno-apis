4777e4de5b6a3327985add1cb175a3df70b31558
Talat Mursalin <tanveer.2387@gmail.com>
2021-09-14 19:23:17 +0600

    upload file limit set

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@ const app = express();
 app.use(morgan('combined'));
 app.use(cors({ origin: process.env.ALLOWED_HOST }))
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '256kb' }));
+app.use(express.urlencoded({ limit: '256kb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // use routes
